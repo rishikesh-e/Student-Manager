f9dfd61bdd9ce135ddfd11d59d27df91e229f9ce
rishikesh-e <rishikesh220707@gmail.com>
2024-12-05 11:47:40 +0530

    Update NavBar.js

--- project frontend/project-client/src/component/common/NavBar.js
+++ project frontend/project-client/src/component/common/NavBar.js
@@ -7,7 +7,7 @@ const NavBar = () => {
 		<nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
 			<div className="container-fluid">
 				<Link className="navbar-brand" to={"/"}>
-					SBR Demo
+					Student Manager 
 				</Link>
 				<button
 					className="navbar-toggler"
